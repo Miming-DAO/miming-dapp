40a54ba7b21ad4ac91028f3422d530a6ffc2e487
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-05-21 18:48:39 +0800

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MIMING CAT - DAPP",
+  description: "The Cross-Chain Cat of Destiny",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#282828]`}>{children}</body>
+    </html>
+  );
+}
