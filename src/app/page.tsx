d9ea579acab233264aae7b2f8d891eea5f62c428
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-05-23 01:16:26 +0800

    added dockerfiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
-const Home = () => {
-  return <></>;
-};
-
-export default Home;
+export default function Home() {
+  redirect('/dapp');
+}
